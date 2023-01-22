10a780f5ddd5f6c389b0a4b83eaba08fb90a4bfc
William <wgumboc@gmail.com>
2023-01-21 18:51:13 -0800

    Fixed card showing logic.

--- frontend/src/WorkoutCard.js
+++ frontend/src/WorkoutCard.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 const WorkoutCard = ({workout}) => {
   return (
-    <div className="workout-card">
-        {workout.show &&
+    <>
+    {workout.show &&
+        <div className="workout-card">
             <div className="workout-text-fields">
             <div>{workout.name}</div>
             <div>Sets: {workout.sets}</div>
             <div>Reps: {workout.reps}</div>
             </div>
-        }
-    </div>
+        </div>
+    }
+    </>
   )
 }
 
